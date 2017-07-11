75fa5983c5af0e2b15f9bbcdfad61808106f6ce7
PaulGuo <badkaikai@gmail.com>
2017-07-11 22:19:01 +0800

    added upload.page.

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage')
+      name: 'upload-page',
+      component: require('@/components/upload.page')
     },
     {
       path: '*',

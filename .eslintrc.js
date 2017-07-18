a40adf0d09a8ac8515980fd8a5df06efcb0c329d
niris <nirisix@gmail.com>
2017-07-18 17:28:45 +0800

    chore: update build config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  globals: {
-    __static: true
-  },
-  plugins: [
-    'html'
-  ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    extends: 'eslint:recommended',
+    plugins: ['html'],
+    rules: {
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
+        'no-constant-condition': [1, { checkLoops: false }],
+        'no-unreachable': 1,
+        'no-unused-vars': [1, { args: 'none' }],
+        semi: process.env.NODE_ENV === 'production' ? 2 : 1,
+        quotes: [
+            1,
+            'single',
+            { avoidEscape: true, allowTemplateLiterals: true }
+        ]
+    }
+};

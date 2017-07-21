b26b55ec428c5d2526c5e2c4c4df746c5d28456a
niris <nirisix@gmail.com>
2017-07-21 20:08:00 +0800

    chore: remove trailing comma

--- build/webpack.renderer.config.js
+++ build/webpack.renderer.config.js
@@ -13,12 +13,12 @@ const env = process.env.NODE_ENV || 'development';
 const common = {
     target: 'electron-renderer',
     entry: {
-        renderer: path.join(__dirname, '../src/renderer/index.js'),
+        renderer: path.join(__dirname, '../src/renderer/index.js')
     },
     output: {
         filename: '[name].js',
         libraryTarget: 'commonjs2',
-        path: path.join(__dirname, '../dist'),
+        path: path.join(__dirname, '../dist')
     },
     module: {
         rules: [
@@ -29,26 +29,26 @@ const common = {
                 use: {
                     loader: 'eslint-loader',
                     options: {
-                        formatter: require('eslint-friendly-formatter'),
-                    },
-                },
+                        formatter: require('eslint-friendly-formatter')
+                    }
+                }
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.html$/,
-                use: 'vue-html-loader',
+                use: 'vue-html-loader'
             },
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                exclude: /node_modules/,
+                exclude: /node_modules/
             },
             {
                 test: /\.node$/,
-                use: 'node-loader',
+                use: 'node-loader'
             },
             {
                 test: /\.vue$/,
@@ -56,39 +56,39 @@ const common = {
                     loader: 'vue-loader',
                     options: {
                         loaders: {
-                            scss: 'vue-style-loader!css-loader!sass-loader',
-                        },
-                    },
-                },
+                            scss: 'vue-style-loader!css-loader!sass-loader'
+                        }
+                    }
+                }
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 use: {
                     loader: 'file-loader',
                     query: {
-                        name: 'imgs/[name].[ext]',
-                    },
-                },
+                        name: 'imgs/[name].[ext]'
+                    }
+                }
             },
             {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 use: {
                     loader: 'file-loader',
                     query: {
-                        name: 'fonts/[name].[ext]',
-                    },
-                },
-            },
-        ],
+                        name: 'fonts/[name].[ext]'
+                    }
+                }
+            }
+        ]
     },
     node: {
         __dirname: process.env.NODE_ENV !== 'production',
-        __filename: process.env.NODE_ENV !== 'production',
+        __filename: process.env.NODE_ENV !== 'production'
     },
     plugins: [
         new webpack.ProgressPlugin(),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env),
+            'process.env.NODE_ENV': JSON.stringify(env)
         }),
         new HtmlWebpackPlugin({
             filename: 'renderer.html',
@@ -96,16 +96,16 @@ const common = {
             minify: {
                 collapseWhitespace: true,
                 removeAttributeQuotes: true,
-                removeComments: true,
-            },
-        }),
+                removeComments: true
+            }
+        })
     ],
     resolve: {
         alias: {
-            '@renderer': path.join(__dirname, '../src/renderer'),
+            '@renderer': path.join(__dirname, '../src/renderer')
         },
-        extensions: ['.js', '.vue', '.json', '.css', '.node'],
-    },
+        extensions: ['.js', '.vue', '.json', '.css', '.node']
+    }
 };
 const dev = webpackMerge(common, {
     devtool: 'eval-source-map',
@@ -113,7 +113,7 @@ const dev = webpackMerge(common, {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.NamedModulesPlugin(),
+        new webpack.NamedModulesPlugin()
     ],
     devServer: {
         host: 'localhost',
@@ -122,24 +122,24 @@ const dev = webpackMerge(common, {
         hot: true,
         stats: {
             children: false,
-            chunks: false,
-        },
-    },
+            chunks: false
+        }
+    }
 });
 const prod = webpackMerge(common, {
     plugins: [
         new BabiliWebpackPlugin({
             removeConsole: true,
-            removeDebugger: true,
+            removeDebugger: true
         }),
         new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),
         new webpack.LoaderOptionsPlugin({
             minimize: true,
-            debug: false,
-        }),
+            debug: false
+        })
     ],
-    bail: true,
+    bail: true
 });
 
 let conf;

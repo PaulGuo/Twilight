65ec13eb147e0932116b21ecaba6b47be32615f3
niris <nirisix@gmail.com>
2017-07-21 14:02:38 +0800

    update: format code

--- src/renderer/views/Upload.vue
+++ src/renderer/views/Upload.vue
@@ -1,80 +1,80 @@
 <style scoped lang="scss">
-.wrapper {
-  height: 100%;
-}
-
-.video {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  .video-content {
-    box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);
-    max-width: 320px;
-    max-height: 480px;
-  }
-
-  .video-btns {
-    text-align: center;
-    margin-top: 25px;
-  }
-}
-
-.upload {
-  position: relative;
-  width: 100%;
-  height: 100%;
-
-  .form {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  .title {
-    text-align: center;
-    margin-bottom: 25px;
-    opacity: 0.3;
-  }
-
-  .footer {
-    position: absolute;
-    bottom: 15px;
-    width: 100%;
-    text-align: center;
-    font-size: 13px;
-    font-family: "Hiragino Kaku Gothic ProN";
-    opacity: 0.5;
-  }
-
-  .upload-container {
-    display: block;
-    position: relative;
-    width: 300px;
-    height: 400px;
-    border: 3px dashed #fff;
-    border-radius: 6px;
-    cursor: pointer;
-    text-align: center;
-    font-size: 50px;
-
-    input {
-      visibility: hidden;
-      display: block;
-      width: 0;
-      height: 0;
+    .wrapper {
+        height: 100%;
     }
 
-    .icon {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
+    .video {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        .video-content {
+            box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);
+            max-width: 320px;
+            max-height: 480px;
+        }
+
+        .video-btns {
+            text-align: center;
+            margin-top: 25px;
+        }
     }
-  }
-}
+
+    .upload {
+        position: relative;
+        width: 100%;
+        height: 100%;
+
+        .form {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        .title {
+            text-align: center;
+            margin-bottom: 25px;
+            opacity: 0.3;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 15px;
+            width: 100%;
+            text-align: center;
+            font-size: 13px;
+            font-family: "Hiragino Kaku Gothic ProN";
+            opacity: 0.5;
+        }
+
+        .upload-container {
+            display: block;
+            position: relative;
+            width: 300px;
+            height: 400px;
+            border: 3px dashed #fff;
+            border-radius: 6px;
+            cursor: pointer;
+            text-align: center;
+            font-size: 50px;
+
+            input {
+                visibility: hidden;
+                display: block;
+                width: 0;
+                height: 0;
+            }
+
+            .icon {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+        }
+    }
 </style>
 
 <template>

2e521ba7d4a66be719e3abb6de07aaf05f3aeb5a
niris <nirisix@gmail.com>
2017-07-27 13:57:00 +0800

    fix: fix require for vue loader 13

--- src/renderer/router.js
+++ src/renderer/router.js
@@ -8,17 +8,17 @@ export default new Router({
         {
             path: '/',
             name: 'upload-page',
-            component: require('@renderer/views/Upload.vue'),
+            component: require('@renderer/views/Upload.vue').default,
         },
         {
             path: '/choose-frames',
             name: 'choose-frames',
-            component: require('@renderer/views/choose-frames/index.vue'),
+            component: require('@renderer/views/choose-frames/index.vue').default,
         },
         {
             path: '/analyse-result',
             name: 'analyse-result',
-            component: require('@renderer/views/analyse-result/index.vue'),
+            component: require('@renderer/views/analyse-result/index.vue').default,
         },
         {
             path: '*',
